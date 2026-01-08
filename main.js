9bd723cb0041401e586bf3d655d0249dc4d00845
A AlShaya <gil63342@gmail.com>
2026-01-08 21:08:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-// تحديد رابط الصفحة الحالية كـ Active
-(function () {
-  const path = location.pathname.split("/").pop() || "index.html";
-  document.querySelectorAll(".menu a").forEach(a => {
-    const href = a.getAttribute("href");
-    if (href === path) a.classList.add("active");
-  });
-})();
+
